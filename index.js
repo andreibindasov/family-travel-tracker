6a66f50f7a921a362738eeee426c3d18bc5dd646
Andrei <bindasov.andrei@gmail.com>
2023-12-06 12:31:51 -0700

    update after migration of postgresql

--- index.js
+++ index.js
@@ -8,17 +8,25 @@ import 'dotenv/config'
 const app = express();
 const port = process.env.PORT;
 
+// const db = new pg.Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_SECRET,
+//   port: process.env.DB_PORT,
+// });
+
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_SECRET,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 db.connect();
 
 var corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   optionsSuccessStatus: 200 
 }
 
