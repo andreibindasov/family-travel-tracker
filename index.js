2ba1e353b479b8eb1fa702427837ae58e2a4b68b
Andrei <bindasov.andrei@gmail.com>
2023-12-06 13:21:25 -0700

    id=5

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let currentUserId = 1;
+let currentUserId = 5;
 
 let users = []
 
